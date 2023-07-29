5184cce512b8082eda8a31ca754ca54f18195afe
Muralidharan Vivekananthan <87437063+muralitherun@users.noreply.github.com>
2023-07-29 06:54:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -99,7 +99,7 @@ app.delete('/tasks/:id', async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
